7fe79607a4b764c19875c102d628342951c503b3
itachi-P <itachip38@gmail.com>
2023-10-08 19:57:13 +0900

    Code division (Building custom widgets)

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,28 +5,35 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.amberAccent,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 87, 9, 223),
-                Color.fromARGB(255, 201, 5, 229)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28.0,
-              ),
-            ),
-          ),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 87, 9, 223),
+            Color.fromARGB(255, 201, 5, 229)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
