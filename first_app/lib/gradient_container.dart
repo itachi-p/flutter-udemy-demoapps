a04fd624bcd6fb84c07101d01a8b206fe52f87eb
itachi-P <itachip38@gmail.com>
2023-10-08 20:57:04 +0900

    refactor:more divide Custom Widgets (define class)

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,34 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/styled_text.dart';
+
+var startAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-
-  // カスタムウィジェットのコンストラクタで名前付き引数keyを親クラスのコンストラクタに渡す
   // const GradientContainer({Key? key}) : super(key: key);
-
   // 上記は頻繁に書くことになり、冗長なので下記のように略記できる
   const GradientContainer({super.key});
 
   @override // アノテーションは省略も可能だが、オーバーライドしていることが明確にできる
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 87, 9, 223),
             Color.fromARGB(255, 201, 5, 229)
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28.0,
-          ),
-        ),
+        child: StyledText()
       ),
     );
   }
