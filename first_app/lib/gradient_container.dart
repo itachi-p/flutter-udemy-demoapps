1e9bea440ffd4d20f4c4e4c41b4e979e917772a1
itachi-P <itachip38@gmail.com>
2023-10-09 17:58:14 +0900

    Introducing Stateful Widgets

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/dice_roller.dart';
+
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
@@ -14,11 +16,6 @@ class GradientContainer extends StatelessWidget {
   final Color color1;
   final Color color2;
 
-  // ボタンを押されたら実行される関数を定義
-  void rollDice() {
-    // ...
-  }
-
   @override
   Widget build(context) {
     return Container(
@@ -29,34 +26,9 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-          child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Image.asset(
-            'assets/images/dice-5.png',
-            width: 200,
-          ),
-          // 位置調整の方法1:画像とボタンの間に別のウィジェットを追加
-          // SizedBoxは指定したサイズに固定。中身のコンテンツのサイズに合わせて伸縮しない
-          const SizedBox(height: 20),
-          TextButton(
-            // ボタンを押されたら実行される関数を指定(実行ではなく、参照先の関数名を指定)
-            onPressed: rollDice,
-            style: TextButton.styleFrom(
-              // 位置調整の方法2:paddingを追加
-              // padding: const EdgeInsets.only(
-              //   top: 20,
-              // ),
-              foregroundColor: Colors.white,
-              textStyle: const TextStyle(
-                fontSize: 28,
-              ),
-            ),
-            child: const Text('Roll Dice'),
-          ),
-        ],
-      )),
+      child: const Center(
+          child: DiceRoller()
+      ),
     );
   }
 }
