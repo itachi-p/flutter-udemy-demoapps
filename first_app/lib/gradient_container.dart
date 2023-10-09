9fcc1b879b07380ff2fa3ebbf3bec947d82b0df7
itachi-P <itachip38@gmail.com>
2023-10-09 17:11:09 +0900

    Styling Buttons & Working with Padding

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -31,14 +31,28 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
           child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset(
             'assets/images/dice-5.png',
             width: 200,
           ),
-          // ボタンを押されたら実行される関数を指定(実行ではなく、参照先の関数名を指定)
+          // 位置調整の方法1:画像とボタンの間に別のウィジェットを追加
+          // SizedBoxは指定したサイズに固定。中身のコンテンツのサイズに合わせて伸縮しない
+          const SizedBox(height: 20),
           TextButton(
+            // ボタンを押されたら実行される関数を指定(実行ではなく、参照先の関数名を指定)
             onPressed: rollDice,
+            style: TextButton.styleFrom(
+              // 位置調整の方法2:paddingを追加
+              // padding: const EdgeInsets.only(
+              //   top: 20,
+              // ),
+              foregroundColor: Colors.white,
+              textStyle: const TextStyle(
+                fontSize: 28,
+              ),
+            ),
             child: const Text('Roll Dice'),
           ),
         ],
