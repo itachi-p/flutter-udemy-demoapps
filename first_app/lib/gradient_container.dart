159ee7d36e8274c9ca28e20953a72bab47763c94
itachi-P <itachip38@gmail.com>
2023-10-09 16:27:11 +0900

    Displaying Images & Using Multiple Constructor Functions

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,38 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/styled_text.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-// class GradientContainer extends StatelessWidget {
-//   // 呼び出し元から複数の色指定をList<Color>型で受け取る
-//   // const GradientContainer(this.colors, {super.key});
-//   // 位置引数の場合は不要だが、名前付き引数はデフォルトではnullになるため、requiredが必要
-//   const GradientContainer({super.key, required this.colors});
-
-//   final List<Color> colors;
-
-//   @override
-//   Widget build(context) {
-//     return Container(
-//       decoration: BoxDecoration(
-//         gradient: LinearGradient(
-//           colors: colors,
-//           begin: startAlignment,
-//           end: endAlignment,
-//         ),
-//       ),
-//       child: const Center(
-//         child: StyledText('This is a New Text.')
-//       ),
-//     );
-//   }
-// }
-
-// リスト型のColorを受け取るのとは別のアプローチ例
-// 名前付き引数ではなく、位置引数で受け取る（2色の指定は自動的に必須になる）
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
+  // デフォルトコンストラクタとは別に、名前付きの複数のコンストラクタを定義
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
 
   final Color color1;
   final Color color2;
@@ -47,8 +24,11 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: const Center(
-        child: StyledText('This is a New Text.')
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-5.png',
+          width: 200,
+        ),
       ),
     );
   }
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 
-// 外部ファイルに切り出したカスタムウィジェットをインポート
 import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        // body: GradientContainer(colors...),
-        //リスト型から2つの位置引数に変更した為、呼び出し元も変更
-        body: GradientContainer(
-          Color.fromARGB(255, 65, 11, 159),
-          Color.fromARGB(255, 127, 36, 176),
-        ),
+        // デフォルトコンストラクタとは別に定義した名前付きコンストラクタを呼び出す
+        body: GradientContainer.purple(),
       ),
     ),
   );
