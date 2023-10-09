e4bc46cd72e4e5fca4da450788909580ee0ef229
itachi-P <itachip38@gmail.com>
2023-10-09 15:00:42 +0900

    refactor:use 'this' keyword

--- first_app/lib/styled_text.dart
+++ first_app/lib/styled_text.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class StyledText extends StatelessWidget {
-  const StyledText({super.key});
+  // 呼び出し元から渡されるテキストをインスタンス変数(プロパティ)で受け取る
+  // const StyledText(String text, {super.key}): outputText = text;
+  // final String outputText;
+
+  // 上記を以下のように簡略に書き換えることもできる（冗長なので通常は下記のように書く）
+  // 変数名は何でもいいが、上記のように区別の為にtextとoutputTextに分ける必要はなくなった
+  const StyledText(this.text, {super.key});
+  final String text;
 
   @override
   Widget build(context) {
-    return const Text(
-      'Hello World',
-      style: TextStyle(
+    return Text(
+      text,
+      style: const TextStyle(
         color: Colors.white,
         fontSize: 28.0,
       ),
