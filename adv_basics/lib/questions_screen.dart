3ef612258e3b742c919f29551885cacd2b59248b
itachi-P <itachip38@gmail.com>
2023-10-13 10:02:03 +0900

    Alignment, Margin & Padding

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -21,25 +21,34 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     return SizedBox(
       // できるだけ横幅いっぱいに広げる
       width: double.infinity,
-      child: Column(
-        // 縦軸の中央に配置する
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.question,
-            style: const TextStyle(color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          // 回答群の文字列リストをAnswerButtonウィジェットのリストに変換する
-          // さらに、childrenが求めているのはList型ではなく単一のウィジェットなので、
-          // map()メソッドの戻り値であるリスト(厳密にはIterable型)を...演算子で展開する
-          ...currentQuestion.answers.map((answer) {
-            return AnswerButton(
-              answerText: answer,
-              onTapped: () {},
-            );
-          }),
-        ],
+      child: Container(
+        // 全ての要素間の余白(マージン)を一律に設定する
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          // Columnの子要素を縦軸の中央に配置する
+          mainAxisAlignment: MainAxisAlignment.center,
+          // 横軸も画面幅いっぱいに伸ばす
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            // 回答群の文字列リストをAnswerButtonウィジェットのリストに変換する
+            // さらに、childrenが求めているのはList型ではなく単一のウィジェットなので、
+            // map()メソッドの戻り値であるリスト(厳密にはIterable型)を...演算子で展開する
+            ...currentQuestion.answers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTapped: () {},
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
