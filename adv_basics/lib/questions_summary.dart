7ef6533ca4e4cc2e857c02e12764fe750f970f08
itachi-P <itachip38@gmail.com>
2023-10-15 00:18:09 +0900

    Making Content Scrollable with ScrollView

--- adv_basics/lib/questions_summary.dart
+++ adv_basics/lib/questions_summary.dart
@@ -1,6 +1,5 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsSummary extends StatelessWidget {
 // results_screenのsummaryをここに移植する
@@ -9,30 +8,47 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // サマリーデータを使って、問題文と回答を表示するウィジェットを作成する
     // Mapオブジェクトのリストから、ウィジェットのリストに変換する
-    return Column(
-      // map()メソッドの引数は、リストの要素を1つずつ受け取る無名関数
-      children: summaryData.map(
-        (data) {
-        // 横長に並べる(Columnの中に入れ子でRowを作成する)
-        return Row(children: [
-          // Map<String, Object>のKeyを指定し、Valueを適切な型にキャストする
-          // 問題番号は0から始まる数値なので、1を足した後で文字列に変換する
-          Text(((data['question_index'] as int) + 1).toString()),
-          // 縦→横→縦とさらに入れ子にする(Column>Row>Column)
-          // はみ出るので、Expandedでスクロール可能にする
-          Expanded(
-            child: Column(children: [
-              Text(data['question_text'] as String),
-              const SizedBox(height: 5),
-              Text(data['user_answer'] as String),
-              Text(data['correct_answer'] as String),
-            ],),
-          ),
-        ],);
-        // 厳密には戻り値がIterable<Widget>なので、toList()でList型ïに変換する
-      },).toList(),
+    return SizedBox(
+      // 高さを固定した上で、スクロール可能にする
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          // map()メソッドの引数は、リストの要素を1つずつ受け取る無名関数
+          children: summaryData.map(
+            (data) {
+              // 横長に並べる(Columnの中に入れ子でRowを作成する)
+              return Row(
+                children: [
+                  // Map<String, Object>のKeyを指定し、Valueを適切な型にキャストする
+                  Text(((data['question_index'] as int) + 1).toString()),
+                  // 縦→横→縦とさらに入れ子にする(Column>Row>Column)
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data['question_text'] as String,
+                            style: GoogleFonts.lato(
+                              color: const Color.fromARGB(255, 224, 126, 244),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            )),
+                        const SizedBox(height: 5),
+                        Text(
+                          data['user_answer'] as String,
+                          style: const TextStyle(color: Colors.green),
+                        ),
+                        Text(data['correct_answer'] as String,
+                            style: const TextStyle(color: Colors.lightBlue)),
+                      ],
+                    ),
+                  ),
+                ],
+              );
+              // 厳密には戻り値がIterable<Widget>なので、toList()でList型ïに変換する
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
