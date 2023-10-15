9ead8eadf1caa90af421c8be53ebd640422c1261
itachi-P <itachip38@gmail.com>
2023-10-15 23:21:52 +0900

    Completed the division of the summary section.

--- adv_basics/lib/questions_summary/questions_summary.dart
+++ adv_basics/lib/questions_summary/questions_summary.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'package:adv_basics/questions_summary/question_identifier.dart';
+import 'package:adv_basics/questions_summary/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
 // results_screenのsummaryをここに移植する
@@ -16,49 +15,13 @@ class QuestionsSummary extends StatelessWidget {
       height: 300,
       child: SingleChildScrollView(
         child: Column(
-          // map()メソッドの引数は、リストの要素を1つずつ受け取る無名関数
-          children: summaryData.map(
-            (data) {
-              // ユーザの回答と正解が一致するかどうかを判定し、変数に格納
-              final isCorrectAnswer =
-                  data['user_answer'] == data['correct_answer'];
-
-              return Row(
-                children: [
-                  // Map<String, Object>のKeyを指定し、Valueを適切な型にキャストする
-
-                  // 正解の場合は青色、不正解の場合は赤色の丸で囲まれた問題番号を表示する
-                  QuestionIdentifier(
-                    questionIndex: data['question_index'] as int,
-                    isCorrectAnswer: isCorrectAnswer,
-                  ),
-                  const SizedBox(width: 10),
-                  // 縦→横→縦とさらに入れ子にする(Column>Row>Column)
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(data['question_text'] as String,
-                            style: GoogleFonts.lato(
-                              color: Colors.white,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        const SizedBox(height: 5),
-                        Text(
-                          data['user_answer'] as String,
-                          style: const TextStyle(color: Colors.green),
-                        ),
-                        Text(data['correct_answer'] as String,
-                            style: const TextStyle(color: Colors.lightBlue)),
-                      ],
-                    ),
-                  ),
-                ],
-              );
-            },
-            // 厳密には戻り値がIterable<Widget>なので、toList()でList型に変換する
-          ).toList(),
+          children:
+              // map()メソッドの引数は、リストの要素を1つずつ取り出し処理する無名関数
+              // 要素を1つづつ引数dataとして渡し、SummaryItem(data)の戻り値をリストに追加する
+              summaryData
+                  .map((data) => SummaryItem(data))
+                  // 厳密にはmap()の戻り値はIterable型なので、List型への変換が必要
+                  .toList(),
         ),
       ),
     );
---
+++ adv_basics/lib/questions_summary/summary_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:adv_basics/questions_summary/question_identifier.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(this.itemData, {super.key});
+  // Object型のValueに対しては、取り出す際に適切な型へキャストが必要
+  final Map<String, Object> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    // ユーザの回答と正解が一致するかどうかを判定し、変数に格納
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+
+    return Row(
+      // 縦軸の先頭(左端)に配置する
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // 正解の場合は青色、不正解の場合は赤色の丸で囲まれた問題番号を表示する
+        QuestionIdentifier(
+          questionIndex: itemData['question_index'] as int,
+          isCorrectAnswer: isCorrectAnswer,
+        ),
+        const SizedBox(width: 20),
+        // 縦→横→縦とさらに入れ子にする(Column>Row>Column)
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(itemData['question_text'] as String,
+                  style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  )),
+              const SizedBox(height: 5),
+              Text(
+                itemData['user_answer'] as String,
+                style: const TextStyle(color: Colors.green),
+              ),
+              Text(itemData['correct_answer'] as String,
+                  style: const TextStyle(color: Colors.lightBlue)),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
