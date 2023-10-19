2b4007b1c8abd7cb5e92ba4a47fd3c6968ac9607
itachi-P <itachip38@gmail.com>
2023-10-19 23:56:34 +0900

    refactor:Organize folders structure

---
+++ 05_ExpenseTrackerApp/expense_tracker/lib/widgets/expenses_list/expenses_item.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// リスト内の個々の要素を表示するウィジェット
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
