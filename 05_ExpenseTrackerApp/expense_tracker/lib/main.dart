caee0c9b41ee64cb9f9e29693622bcf2cd9f6df4
itachi-P <itachip38@gmail.com>
2023-10-29 20:31:21 +0900

    Getting Started with Theming

--- 05_ExpenseTrackerApp/expense_tracker/lib/main.dart
+++ 05_ExpenseTrackerApp/expense_tracker/lib/main.dart
@@ -5,7 +5,13 @@ import 'package:expense_tracker/widgets/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      // アプリ全体に影響を与える共通構成をここで設定
+      // テーマ設定(色、テキストスタイルなどの組み合わせ)もその一つ
+      // ThemeData()そのものには引数を渡さずcopyWith()で引数を渡すと、
+      // 既存のデフォルト設定に引数で渡した値の差分だけをオーバーライドする形でテーマを設定できる
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          scaffoldBackgroundColor: const Color.fromARGB(255, 237, 145, 244)),
       home: const Expenses(),
     ),
   );
