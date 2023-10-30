87980cfd916a66fe01f882593b65f11172165431
itachi-P <itachip38@gmail.com>
2023-10-30 11:59:45 +0900

    Adding Chart Widgets

--- 05_ExpenseTrackerApp/expense_tracker/lib/widgets/expenses.dart
+++ 05_ExpenseTrackerApp/expense_tracker/lib/widgets/expenses.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
-import 'package:expense_tracker/widgets/new_expense.dart';
+import 'package:expense_tracker/widgets/chart/chart.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -112,8 +113,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
-          // TODO: Add Chart Widget
-          const Text('The Chart'),
+          Chart(expenses: _registeredExpense),
           //実際に経費データを表示するListウィジェット
           // Columnの中でListViewのような可変長ウィジェットはExpandedで包む
           Expanded(
