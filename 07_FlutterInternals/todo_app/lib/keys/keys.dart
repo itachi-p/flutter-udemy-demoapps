e7a072c7649cd85f47fca83226bf41e2545b0c48
itachi-P <itachip38@gmail.com>
2023-11-10 20:07:20 +0900

    Which problem do keys solve?

--- 07_FlutterInternals/todo_app/lib/keys/keys.dart
+++ 07_FlutterInternals/todo_app/lib/keys/keys.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_internals/keys/todo_item.dart';
+// import 'package:flutter_internals/keys/todo_item.dart';
+import 'package:flutter_internals/keys/checkable_todo_item.dart';
 
 // データモデル
 class Todo {
@@ -72,7 +73,12 @@ class _KeysState extends State<Keys> {
             children: [
               // {}波括弧無しループでリストに要素を追加（※前項参照）
               for (final todo in _orderedTodos)
-                TodoItem(
+                // TodoItem(
+                // チェックボックスのstateは他の要素の並び替え順と整合していない
+                // この理由は、widgetが移動(elementからのポインタ参照先が変わる)しても
+                // element及びstateはそのまま再利用される(移動しない)ため
+                // これを合わせるには、keyを使う
+                CheckableTodoItem(
                   todo.text,
                   todo.priority,
                 ),
