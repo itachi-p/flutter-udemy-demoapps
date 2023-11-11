d393fe67e0d19267f6e30cf1d6f4800b7a1792f0
itachi-P <itachip38@gmail.com>
2023-11-11 23:48:41 +0900

    Mutating values in memory & Make sense of modifier

--- 07_FlutterInternals/todo_app/lib/main.dart
+++ 07_FlutterInternals/todo_app/lib/main.dart
@@ -4,6 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
+  // finalで宣言した変数に「再代入」はできない
+  final numbers = [1, 2, 3];
+  // これは別のメモリアドレスを割り当てる操作になるため不可
+  // numbers = [4, 5, 6];
+
+  // ただしここで変数はリストのメモリ番地を指すポインタであるため、
+  // ポインタが指す先の同じメモリアドレスにある要素の変更は可能
+  // constはいずれも不可。コンパイルエラーは出ないが、実行時エラーになる
+  numbers.add(4);
+  numbers[0] = 5;
+  print(numbers);
+
   runApp(const App());
 }
 
