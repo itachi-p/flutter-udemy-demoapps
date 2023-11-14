1489fe904071d04e1670ff0435c6ccd87d736297
itachi-P <itachip38@gmail.com>
2023-11-14 21:26:54 +0900

    Add meals screen and testing empty meals list

--- 08_NavigationMultiScreens/meals_app/lib/main.dart
+++ 08_NavigationMultiScreens/meals_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/meals.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -24,7 +25,14 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const CategoriesScreen(),
+      // home: const CategoriesScreen(),
+      // テスト表示
+      home: const MealsScreen(
+        // とりあえずダミーのテキストをハードコーディングしておく
+        title: 'Some category ...',
+        // fallbackコンテンツが正常に表示されるか確認する為に、空のリストを渡す
+        meals: [],
+      ),
     );
   }
 }
--- 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
+++ 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
@@ -14,6 +14,34 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // mealsの中身が空の場合に備え、場合分けする
+    Widget content = ListView.builder(
+      itemBuilder: (cxt, index) => Text(
+        meals[index].title,
+      ),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // テーマカラーを基に、背景色に合わせた文字色を自動で設定
+            Text('Uh oh ... nothing here!', style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
     // カテゴリー分類された料理リストを表示する
     return Scaffold(
       appBar: AppBar(
@@ -21,11 +49,7 @@ class MealsScreen extends StatelessWidget {
       ),
       // スクロール可能、かつ動的に可視部分だけのウィジェット生成でパフォーマンス最適化
       // itemBuilderプロパティには、contextとindexを引数にとり、ウィジェットを返す関数を渡す
-      body: ListView.builder(
-        itemBuilder: (cxt, index) => Text(
-          meals[index].title,
-        ),
-      ),
+      body: content,
     );
   }
 }
