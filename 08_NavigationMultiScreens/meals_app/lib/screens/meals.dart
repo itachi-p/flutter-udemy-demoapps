9aac2bd10a71ea242c45001505a8e1f5afb88290
itachi-P <itachip38@gmail.com>
2023-11-14 21:45:07 +0900

    style:Correct position of comments and line breaks

--- 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
+++ 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
@@ -12,27 +13,31 @@ class MealsScreen extends StatelessWidget {
   final String title;
   final List<Meal> meals;
 
+  // カテゴリー分類された料理リストを表示する画面
   @override
   Widget build(BuildContext context) {
-    // mealsの中身が空の場合に備え、場合分けする
+    // スクロール可能、かつ動的に可視部分だけのウィジェット生成でパフォーマンス最適化
     Widget content = ListView.builder(
       // itemCountプロパティに、リストの要素数を渡す
       // これがないと、ListViewを正しくレンダリングできない
       itemCount: meals.length,
-      itemBuilder: (cxt, index) => Text(
-        meals[index].title,
-      ),
+      // itemBuilderプロパティには、contextとindexを引数にとり、ウィジェットを返す関数を渡す
+      itemBuilder: (cxt, index) => Text(meals[index].title),
     );
 
+    // mealsの中身が空の場合に備え、場合分けする
     if (meals.isEmpty) {
       content = Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             // テーマカラーを基に、背景色に合わせた文字色を自動で設定
-            Text('Uh oh ... nothing here!', style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+            Text(
+              'Uh oh ... nothing here!',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground,
-                  ),),
+                  ),
+            ),
             const SizedBox(height: 16),
             Text(
               'Try selecting a different category!',
@@ -45,13 +50,10 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
-    // カテゴリー分類された料理リストを表示する
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      // スクロール可能、かつ動的に可視部分だけのウィジェット生成でパフォーマンス最適化
-      // itemBuilderプロパティには、contextとindexを引数にとり、ウィジェットを返す関数を渡す
       body: content,
     );
   }
