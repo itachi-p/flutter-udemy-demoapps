f475f39fcd63e526893a7ecd8e4c8295f402536b
itachi-P <itachip38@gmail.com>
2023-11-14 19:47:12 +0900

    Making any widget tappable with InkWell

--- 08_NavigationMultiScreens/meals_app/lib/widgets/category_grid_item.dart
+++ 08_NavigationMultiScreens/meals_app/lib/widgets/category_grid_item.dart
@@ -12,25 +12,33 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    // Container以下をタップ可能にする為にInkWellでラップ
+    // GestureDetectorでも様々なリスナーを設定できるが、InkWellは視覚的なフィードバックが付く
+    return InkWell(
+      onTap: () {},
+      splashColor: Theme.of(context).primaryColor, // タップ時の色効果
+      borderRadius: BorderRadius.circular(16), // 角を丸くする
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16), // 角を丸くする
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        // スタイルはテーマから取得するが、copyWithで一部変更する為にnull許容型にする
-        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-              // mainで設定したベースカラーを基に、背景色に合わせた文字色を自動で設定
-              color: Theme.of(context).colorScheme.onBackground,
-            ),
+        child: Text(
+          category.title,
+          // スタイルはテーマから取得するが、copyWithで一部変更する為にnull許容型にする
+          style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                // mainで設定したベースカラーを基に、背景色に合わせた文字色を自動で設定
+                color: Theme.of(context).colorScheme.onBackground,
+              ),
+        ),
       ),
     );
   }
