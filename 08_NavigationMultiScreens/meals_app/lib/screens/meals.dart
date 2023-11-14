4bd6fedf2de25ffe31cf2426abded2835a3121d2
itachi-P <itachip38@gmail.com>
2023-11-14 20:52:55 +0900

    Add title and meals list to screen

--- 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
+++ 08_NavigationMultiScreens/meals_app/lib/screens/meals.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({
+    super.key,
+    required this.title,
+    required this.meals,
+  });
+
+  // 前の画面で選択されたカテゴリーを受け取る
+  final String title;
+  final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
     // カテゴリー分類された料理リストを表示する
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: 
+      // DUMMY: 仮のウィジェット
+      const Center(
+        child: Text('The meals for the category!'),
+      ),
+    );
   }
 }
