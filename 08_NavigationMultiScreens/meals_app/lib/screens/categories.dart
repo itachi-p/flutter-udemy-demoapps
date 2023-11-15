142573b82aa26f6dd256a1d17ba27c4c90286b65
itachi-P <itachip38@gmail.com>
2023-11-15 23:55:43 +0900

    Passing Data to the Target Screen

--- 08_NavigationMultiScreens/meals_app/lib/screens/categories.dart
+++ 08_NavigationMultiScreens/meals_app/lib/screens/categories.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 
 import 'package:meals_app/data/dummy_data.dart';
-import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
+import 'package:meals_app/screens/meals.dart';
+import 'package:meals_app/models/category.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  // 特定のカテゴリーがタップされた時の処理
+  void _selectCategory(BuildContext context, Category category) {
+    // 渡されたカテゴリーと一致するMealを抽出
+    // 1件づつ真偽値でフィルタリングし、結果がtrueのみの新しいリストを変数に格納
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList(); // where()はIterable型を返すメソッドなので、List型に再変換
+
     // Navigator.popとは逆に、画面スタックの一番上に新しい画面を押し込む
     Navigator.of(context).push(
       // Navigator.push(content, route)でも同じ
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'Some title',
-          meals: [],
+          // 渡されたカテゴリーの情報を動的に生成
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -45,7 +54,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              onSelectCategory: () => _selectCategory(context),
+              onSelectCategory: () => _selectCategory(context, category),
             ),
         ],
       ),
